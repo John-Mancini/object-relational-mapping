69f2c22a7ccfe41b9719eac2a714fca06631b073
John-Mancini <mancinijohn5988@gmail.com>
2021-06-02 20:26:26 -0400

    finished

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -139,6 +139,13 @@ router.put("/:id", (req, res) => {
 
 router.delete("/:id", (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({ where: { id: req.params.id } })
+    .then(function (category) {
+      if (category) {
+        res.send("Successfully deleted Product!");
+      }
+    })
+    .catch((err) => res.json(err));
 });
 
 module.exports = router;
